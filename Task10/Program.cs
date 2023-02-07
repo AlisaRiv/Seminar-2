3b2a84ecf3686d6698d6abae34e86ea99fd10577
Nadezhda Bereza <a-river@bk.ru>
2023-02-07 14:27:46 +0300

    Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число
+ //и на выходе показывает вторую цифру этого числа
+
+Console.Clear();
+Console.Write("Введите трехзначное число: ");
+string number = Console.ReadLine();
+int n = Convert.ToInt32(number);
+if (n > 99 && n < 1000) {
+    Console.WriteLine($"{n} -> {number[1]}");
+} else {
+    Console.WriteLine("Необходимо ввести трехзначное число.");
+}
